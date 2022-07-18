affbf2dc9e5f5664d4e569b0b7418c50f5ec07d6
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-18 18:32:42 +0300

    Zadacha41

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ($"Задайте количеcтво чисел, которые будут введены:");
+int M = Convert.ToInt32(Console.ReadLine());
+void FillNumber ( int M)
+{
+    int count = 0;
+    for (int i = 1; i < M + 1; i++)
+    {
+        Console.WriteLine ($"Введите число {i}:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0) count++;
+    }
+
+    Console.WriteLine("Количество чисел больше нуля = " + count);
+}
+FillNumber (M);
